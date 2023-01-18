83d535b83f53c023c37de1fe491ee8a16edab81b
Akshara Mishra <akshara.skyrockets@gmail.com>
2023-01-18 12:07:58 +0530

    add create quize apge

--- src/app/create-quizzo/page.js
+++ src/app/create-quizzo/page.js
@@ -5,21 +5,21 @@ import { BsImageFill } from "react-icons/bs";
 import { IoClose } from "react-icons/io5";
 import styled from "styled-components";
 
-function ImageCollection() {
+function CreateQuizzo() {
   const [file, setFile] = useState();
   const handelFile = (e) => {
     console.log(e.target.files);
     setFile(URL.createObjectURL(e.target.files[0]));
   };
   return (
-    <ImageCollection1>
+    <CreateQuizzo1>
       <div className="flex items-center gap-4 header">
         <Link href="/home">
           <div>
             <IoClose className="text-2xl" />
           </div>
         </Link>
-        <div className="font-bold text-[20px]">Create New Collection</div>
+        <div className="font-bold text-[20px]">Create Quizzo</div>
       </div>
 
       <div className="border-2 mt-10 border-[#8166ff] overflow-hidden  rounded-2xl h-60 relative">
@@ -46,6 +46,37 @@ function ImageCollection() {
           placeholder="enter your email address"
         />
       </div>
+
+      <div className="mt-10">
+        <p>Description</p>
+        <input
+          className="input font-bold"
+          id="message"
+          name="message"
+          type="text"
+          placeholder="enter your email address"
+        />
+      </div>
+      <div className="mt-10">
+        <p>Select Collection</p>
+        <select className="font-bold">
+          <option>Eduction</option>
+          <option>Food</option>
+          <option>Music</option>
+          <option>Saving</option>
+          <option>Business</option>
+          <option>Motivation</option>
+        </select>
+      </div>
+
+      <div className="mt-10">
+        <p>Select Theme</p>
+        <select className="font-bold">
+          <option>Quizoo Default</option>
+          <option>None</option>
+        </select>
+      </div>
+
       <div className="mt-10">
         <p>Visible to</p>
         <select className="font-bold">
@@ -54,18 +85,46 @@ function ImageCollection() {
           <option>Public</option>
         </select>
       </div>
-      <hr />
+
+      <div className="mt-10">
+        <p>Visible to Quiz Questions</p>
+        <select className="font-bold">
+          <option>Only Me</option>
+          <option>Friends of Friends</option>
+          <option>Public</option>
+        </select>
+      </div>
+
+      <div className="mt-10">
+        <p>Keywords</p>
+        <input
+          className="input font-bold"
+          id="message"
+          name="message"
+          type="text"
+          placeholder="Type Keyword and enter ex: learning games"
+        />
+      </div>
+
+      <div className="flex items-center justify-center">
       <Link href="/home">
-        <div className="createBtn">
-          <button>Create</button>
+        <div className="saveBtn">
+          <button>Save</button>
         </div>
       </Link>
-    </ImageCollection1>
+
+      <Link href="/home">
+        <div className="createBtn">
+          <button>Add Question</button>
+        </div>
+      </Link>
+      </div>
+    </CreateQuizzo1>
   );
 }
 
-export default ImageCollection;
-const ImageCollection1 = styled.div`
+export default CreateQuizzo;
+const CreateQuizzo1 = styled.div`
   padding-bottom: 60px;
   .card-image {
     width: 350px;
@@ -80,8 +139,18 @@ const ImageCollection1 = styled.div`
     box-shadow: #543acc 0px 5px 0px;
     margin: 55px 20px;
     border-radius: 50px;
-    padding: 10px;
+    padding: 10px ;
     color: var(--whiteColor);
+    text-align: center;
+    font-weight: 600;
+  }
+  .saveBtn{
+    background-color:#f0edff;
+    box-shadow: #c4b7ff 0px 5px 0px;
+    margin: 55px 20px;
+    border-radius: 50px;
+    padding: 10px 40px;
+    color: var(--blueColor);
     text-align: center;
     font-weight: 600;
   }
